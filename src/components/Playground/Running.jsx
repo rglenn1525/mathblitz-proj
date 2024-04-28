6bbb1c852c3806d9eb3d4f17d2959884a405779e
Rean Glenn Roquero <reanglenn08@gmail.com>
2024-04-28 23:55:41 +0800

    k

--- src/components/Playground/Running.jsx
+++ src/components/Playground/Running.jsx
@@ -3,12 +3,12 @@ import { useState } from 'react'
 import './Play.css'
 
 export default function Running({setIsTimeout, currentItem}) {
-    const [timeLeft, setTimeLeft] = useState(10);
+    const [timeLeft, setTimeLeft] = useState(20);
 
     return <>
     <div className='running' style={{backgroundColor: "grey"}}>
-    <div className='runningtime' style={{width: ((timeLeft-1)/10)*100 + "%"}}>
-        <Countdown time="10" onTimeEnd={()=>setIsTimeout("grid")} setTimeLeft={setTimeLeft} currentItem={currentItem}/>
+    <div className='runningtime' style={{width: ((timeLeft-1)/20)*100 + "%"}}>
+        <Countdown time="20" onTimeEnd={()=>setIsTimeout("grid")} setTimeLeft={setTimeLeft} currentItem={currentItem}/>
     </div>
     </div>
     </>
